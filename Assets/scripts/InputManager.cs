831fe8e1b4e4d9f9a471f9cdf0b0805d82cd7321
stu <stumcg@student.ubc.ca>
2025-02-04 22:52:05 -0800

    player movement

--- Assets/scripts/InputManager.cs
+++ Assets/scripts/InputManager.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour {
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             OnSpacePressed?.Invoke();
         }
+        Vector2 input = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
